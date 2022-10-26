5e8475b1a8381442094af715241af2763d1b7861
Bondok6 <kirolousbondok6@gmail.com>
2022-10-26 05:17:52 +0200

    Add user info

---
+++ js/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Get username from URL query string
+const getUsername = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const username = urlParams.get('username');
+  return username;
+};
+
+// Get user info
+const getUser = async (username) => {
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// render user info
+const renderUser = (user) => {
+  let userHTML = `
+    <img src="${user.avatar_url}" alt="avatar" class="user__info">
+    <div class="user-info">
+      <h2>${user.login}</h2>
+      <span>Bio: ${user.bio || 'Not available'}</span>
+      <span>Followers: ${user.followers}</span>
+      <span>Following: ${user.following}</span>
+      <span>Public Repos: ${user.public_repos}</span>
+      <span>Location: ${user.location}</span>
+      <span>Company: ${user.company}</span>
+      <span>Twitter: ${user.twitter_username || 'Not available'}</span>
+    </div>
+  `;
+
+  document.querySelector('.user').insertAdjacentHTML('afterbegin', userHTML);
+};
+
+// Display user on page load
+(async () => {
+  const username = getUsername();
+  const user = await getUser(username);
+  console.log(user);
+  renderUser(user);
+})();
